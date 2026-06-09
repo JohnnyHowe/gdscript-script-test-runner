fe0405afbd8ce5f9bfd687e16e1d99002adf732c
Johnny Howe <jonathon.o.howe@gmail.com>
2026-06-09 23:21:35 +1200

    Use test file loader in simplified discovery

--- src/discovery_simplified/test_discovery.gd
+++ src/discovery_simplified/test_discovery.gd
@@ -1,12 +1,10 @@
 ## Public entry point for the test discovery namespace.
 ## Finds tests without running them and can adapt discovered data for the existing runner.
 const _Configuration := preload("../configuration.gd")
-const _TestDataObjects := preload("../test_data_objects/main.gd")
 
 const TestSuite := preload("./data/test_suite.gd")
-const DiscoveryJson := preload("./output/discovery_json.gd")
 const TestFileFinder := preload("./scanning/test_file_finder.gd")
-const GDScriptMethodLineIndex := preload("./parsing/gdscript_method_line_index.gd")
+const TestFileLoader := preload("./reading/test_file_loader.gd")
 
 var _configuration: _Configuration
 var _file_finder: TestFileFinder
@@ -21,6 +19,6 @@ func discover() -> TestSuite:
 	var suite := TestSuite.new()
 
 	for script: GDScript in _file_finder.find():
-		pass
+		suite.files.append(TestFileLoader.new(script, _configuration).load())
 
 	return suite
