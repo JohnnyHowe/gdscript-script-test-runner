f7c4af64c43cea37ac314d496b5f9306094f8ed2
Johnny Howe <jonathon.o.howe@gmail.com>
2026-06-09 19:11:00 +1200

    Created example test

---
+++ example/example.tests.gd
@@ -0,0 +1,7 @@
+
+## Run with following command
+## godot --headless -s addons/gdscript-script-test-runner/src/run_tests.gd -- test_file_pattern=example/example.tests.gd test_name_pattern=test_one_plus_one_equals_two hide_passed_tests=false
+func test_one_plus_one_equals_two() -> ScriptTestResult:
+	var expected := 2
+	var actual := 1 + 1
+	return ScriptTestResult.from_equals(expected, actual)
