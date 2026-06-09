0905b1a3c075ddf320c69902fa8b5d519e8469ee
Johnny Howe <jonathon.o.howe@gmail.com>
2026-06-10 01:28:09 +1200

    Remove unused results preload hub

--- src/data/results/main.gd
+++
@@ -1,4 +0,0 @@
-const TestCaseResult := preload("./test_case_result.gd")
-const TestFileResult := preload("./test_file_result.gd")
-const TestSuiteResult := preload("./test_suite_result.gd")
-const TestScriptsLogCreator := preload("../logging/log.gd")
