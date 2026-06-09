aa0a0607c12467b8927595a5e551961fc04522fc
Johnny Howe <jonathon.o.howe@gmail.com>
2026-06-09 22:43:55 +1200

    Created run request structure

---
+++ src/run_request/file_run_request.gd
@@ -0,0 +1,4 @@
+const CaseRequest := preload("./case_run_request.gd")
+
+
+var file_path: StringName
---
+++ src/run_request/suite_run_request.gd
@@ -0,0 +1,5 @@
+const FileRunRequest := preload("./file_run_request.gd")
+const CaseRequest := FileRunRequest.CaseRequest
+
+
+var files: Array[FileRunRequest]
