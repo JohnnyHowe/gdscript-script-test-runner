da7a07ca761b9471a5fadfc82c0099ad28c0f3d2
Johnny Howe <jonathon.o.howe@gmail.com>
2026-06-11 09:55:13 +1200

    bat install script now installs node modules

--- install_vscode_extension.bat
+++ install_vscode_extension.bat
@@ -1,4 +1,17 @@
 @echo off
 pushd "%~dp0"
-npm --prefix vscode-extension run install:local
+
+if not exist "vscode-extension\node_modules\.bin\tsc.cmd" (
+	call npm --prefix vscode-extension ci
+	if errorlevel 1 (
+		set EXIT_CODE=%errorlevel%
+		popd
+		exit /b %EXIT_CODE%
+	)
+)
+
+call npm --prefix vscode-extension run install:local
+set EXIT_CODE=%errorlevel%
+
 popd
+exit /b %EXIT_CODE%
